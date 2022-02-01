610f773a79705806d82ee5d31540b62b1381d58b
Slavka <slavkabest1993@gmail.com>
2022-02-01 19:51:43 +0200

    module 1 / task 4

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,10 @@
 function checkSpam(str) {
-  // ваш код...
+  let lowerStr = str.toLowerCase();
+  if (lowerStr.indexOf("1xbet") != -1 || lowerStr.indexOf("xxx") != -1) {
+    return true;
+  }
+  return false;
 }
+checkSpam('1XbeT now') === true
+checkSpam('free xxxxx') === true
+checkSpam('innocent rabbit') === false
