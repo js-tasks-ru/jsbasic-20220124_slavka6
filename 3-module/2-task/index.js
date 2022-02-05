e687f4111f0aa2bfb79adea57194657b9fae2ddb
Slavka <slavkabest1993@gmail.com>
2022-02-05 10:29:39 +0200

    module-2/task-2

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,12 @@
+let arr = [5, 3, 8, 1];
+
 function filterRange(arr, a, b) {
-  // ваш код...
-}
+  let result;
+  result = arr.filter((item => item >= a && item <= b));
+  return result;
+};
+
+let filtered = filterRange(arr, 1, 4);
+
+console.log( filtered ); // [3,1] (совпадающие значения)
+console.log( arr ); // [5,3,8,1] (без изменений)
