7ba4be4f50b0f8b7464d437a57bdcd01e7125163
Slavka <slavkabest1993@gmail.com>
2022-02-05 11:54:46 +0200

    module-3/fc-ing3 task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,17 @@
 function camelize(str) {
-  // ваш код...
+  let newStr = str.split('-');
+  newStr = newStr.map((item, index) => {
+    if (index === 0) {
+      return item;
+    }
+    return item[0].toUpperCase() + item.slice(1);
+  });
+  return newStr.join('');
 }
+
+
+camelize('background-color') == 'backgroundColor';
+camelize('list-style-image') == 'listStyleImage';
+camelize('-webkit-transition') == 'WebkitTransition';
+
+  // console.log(upperStr);
