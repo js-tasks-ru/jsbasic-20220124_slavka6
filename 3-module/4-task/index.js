250c760f20623842bb56654a56ddaedefab9df11
Slavka <slavkabest1993@gmail.com>
2022-02-06 22:59:35 +0200

    task--4

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,8 @@
 function showSalary(users, age) {
-  // ваш код...
-}
+  let resultFiltered = users.filter(item => item.age <= age).map(item => `${item.name}, ${item.balance}`).join(', \n');
+  return resultFiltered;
+};
+
+
+let result = showSalary(users, age);
+
