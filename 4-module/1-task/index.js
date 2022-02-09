54b96b9b02c5f8bbd6bc307ff3d28c8424b3cc95
SlavkaSlavka <56847146+SlavkaSlavka@users.noreply.github.com>
2022-02-09 13:50:11 +0200

    module-4/task-1

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
-function makeFriendsList(friends) {
-  // ваш код...
+function makeFriendsList(friends) {  
+  const ul = document.createElement('ul');
+  const body = document.body;
+  for (friend of friends) {
+    let li;
+    li = document.createElement('li');
+    li.innerHTML = `${friend.firstName} ${friend.lastName}`;
+    ul.appendChild(li);
+  } 
+  return ul;
 }
