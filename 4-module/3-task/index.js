42994de9ddb1ef4ae80c758f699d6127b5bf6477
SlavkaSlavka <56847146+SlavkaSlavka@users.noreply.github.com>
2022-02-09 16:18:14 +0200

    module4/task-3

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,22 @@
 function highlight(table) {
-  // ваш код...
+  for (let i = 0; i < table.rows.length; i++) {
+    if (table.rows[i].cells[1].textContent <= 18) {
+      table.rows[i].style.textDecoration = 'line-through';
+    }
+
+    if (table.rows[i].cells[3].getAttribute('data-available') === 'true') {
+      table.rows[i].classList.add('available');
+    } else if (table.rows[i].cells[3].getAttribute('data-available') === 'false') {
+    table.rows[i].classList.add('unavailable');
+    } else if (table.rows[i].cells[3].getAttribute('data-available') === null) {
+      table.rows[i].setAttribute('hidden', 'true');
+    }   
+    
+    
+    if (table.rows[i].cells[2].textContent.toLowerCase() === 'm') {
+      table.rows[i].classList.add('male');
+    } else if (table.rows[i].cells[2].textContent.toLowerCase() === 'f'){
+           table.rows[i].classList.add('female');
+    }  
+  }
 }
