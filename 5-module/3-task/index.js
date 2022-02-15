9b7132413d9a2cf5d34161ebbb504fdc1d203bd2
SlavkaSlavka <56847146+SlavkaSlavka@users.noreply.github.com>
2022-02-15 14:52:41 +0200

    module-5/ task-3

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,27 @@
+
 function initCarousel() {
-  // ваш код...
+  const innerDiv = document.querySelector('.carousel__inner');
+  const leftArrow = document.querySelector('.carousel__arrow_left');
+  const rightArrow = document.querySelector('.carousel__arrow_right');
+  let slide = 0;
+  let lastSlide = document.querySelectorAll('.carousel__slide').length;
+  leftArrow.style.display = 'none';
+  
+  rightArrow.addEventListener('click', () => { 
+    innerDiv.style.transform = `translateX(-${(slide + 1) * innerDiv.offsetWidth}px)`;
+    slide += 1;
+  leftArrow.style.display = '';
+    if(slide === lastSlide - 1) {
+      rightArrow.style.display = 'none';
+    }
+  });
+
+  leftArrow.addEventListener('click', () => {
+    innerDiv.style.transform = `translateX(-${(slide - 1) * innerDiv.offsetWidth}px)`;
+    slide -= 1;
+    rightArrow.style.display = '';
+    if(slide === 0) {
+      leftArrow.style.display = 'none';
+    }
+  });
 }
